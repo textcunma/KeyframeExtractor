d21aeef6b6dd9c44654d44e6dbcda5e1f7b2ccdc
textcunma <78415309+textcunma@users.noreply.github.com>
2023-01-24 04:28:06 +0900

    modified view process:(prevent never end loop process))

--- src/components/VideoView.vue
+++ src/components/VideoView.vue
@@ -1,6 +1,6 @@
 <template>
      <div class = "border-dashed border-4 rounded-2xl border-rose-300 w-80 h-45 mx-auto">
-          <video id = "displayVideo" src = "" hidden class = "w-80 mx-auto rounded-2xl" autoplay controls></video>
+          <video id = "displayVideo" src = "" hidden class = "w-80 mx-auto rounded-2xl" autoplay></video>
      </div>
 
      <div id = "inputArea" class = "flex justify-center items-center my-8 cursor-pointer 
--- src/cvprocess.ts
+++ src/cvprocess.ts
@@ -1,34 +1,35 @@
 import cv, { Mat } from "opencv-ts";
 import type {} from 'typed-query-selector/strict'
 
-function component() {
-     const element = document.createElement('canvas');
-     return element;
-}
-
 function getVideo(){
      const runBtn = document.querySelectorAll('#runArea');
      const video = document.getElementById('displayVideo') as HTMLVideoElement;
      const result = document.getElementById("result") as HTMLDivElement;
 
+     const fps = 1000 / 30;   // 30 FPS
+
      runBtn.forEach((ele) => {
           ele.addEventListener('click', () => {
-               
+     
                // const src: Mat = new cv.Mat(video.height, video.width, cv.CV_8UC4);
                // const dst: Mat = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+               
                let i = 0;
-               setInterval(function() {
-                    const canvas:any = component();
+               const timeid = setInterval(function() {
+                    if(video.ended){
+                         clearInterval(timeid);
+                    }
+                    const canvas:any = document.createElement('canvas');
                     const context = canvas.getContext('2d');
 
                     context.drawImage(video, 0, 0, canvas.width, canvas.height);
                     context.fillStyle = 'rgb(0, 0, 0)';
-                    context.fillRect(canvas.width * 0.9, canvas.height*0.9, canvas.width * 0.95, canvas.height * 0.95);
+                    context.fillRect(canvas.width * 0.9, canvas.height * 0.9, 
+                                        canvas.width * 0.95, canvas.height * 0.95);
                     context.font = '10px serif';
                     context.fillStyle = 'rgb(255, 255, 255)';
                     context.fillText(String(i), canvas.width * 0.92 , canvas.height * 0.98);
 
-                    // OpenCV.jsを使用して画像をグレースケールに変換
                     const src: Mat = cv.imread(canvas);
                     cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY);
                     cv.imshow(canvas, src);
@@ -37,7 +38,7 @@ function getVideo(){
                     result.appendChild(canvas);
                     i += 1
 
-          }, 50);
+               }, fps);
           })
      })
 }
