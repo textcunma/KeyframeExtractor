37d805f8979dac68b9415d6927b5cc20bdad9e69
textcunma <78415309+textcunma@users.noreply.github.com>
2023-01-25 11:30:10 +0900

    add calc Histogram

--- src/cvprocess.ts
+++ src/cvprocess.ts
@@ -1,6 +1,19 @@
 import cv, { Mat } from "opencv-ts";
 import type {} from 'typed-query-selector/strict'
 
+const channels = [0];
+const histSize = [256];
+const ranges = [0, 256];
+
+// function getHistgram(image : Mat){
+//      const grayimage = new cv.Mat();
+//      cv.cvtColor(image, grayimage, cv.COLOR_RGBA2GRAY);
+//      const hist = new cv.Mat();
+//      cv.calcHist(grayimage, channels, new cv.Mat(), hist, histSize, ranges);
+//      grayimage.delete();
+//      return hist
+// }
+
 function mainprocess(){
      const video = document.getElementById('displayVideo') as HTMLVideoElement;
      const result = document.getElementById("result") as HTMLDivElement;
@@ -22,11 +35,17 @@ function mainprocess(){
      video.controls = false;
      
      let i = 1;
+     let similarity = 0.0;
+
+     const hist: Mat = new cv.Mat(height, width, cv.CV_8UC1);
+     let hist2: Mat = new cv.Mat(height, width, cv.CV_8UC1);
 
      const timeid = setInterval(function() {
           if(video.ended || duration - video.currentTime < 0.01){
                clearInterval(timeid);
                video.controls = true;
+               hist.delete();
+               hist2.delete();  
           }
           // 現在のフレームの読み込みが完了したら更新
           if (video.readyState > 3) {
@@ -38,39 +57,61 @@ function mainprocess(){
                canvas.height = height;
                const context = canvas.getContext('2d', {willReadFrequently: true}) as CanvasRenderingContext2D;
      
-               const src: Mat = new cv.Mat(height, width, cv.CV_8UC4);
-     
                // link image
                context.drawImage(video, 0, 0, width, height);
-     
-               // add frame number
-               context.fillStyle = 'rgb(0, 0, 0)';
-               context.fillRect(width * 0.9, height * 0.9, 
-                                   width * 0.95, height * 0.95);
-               context.font = '10px serif';
-               context.fillStyle = 'rgb(255, 255, 255)';
-               context.fillText(String(i), width * 0.92 , height * 0.98);
-               
+                    
                // set
+               const src: Mat = new cv.Mat(height, width, cv.CV_8UC4);
                src.data.set(context.getImageData(0, 0, width, height).data);
+                              
+               // RGBA -> GRAY
+               const gray: Mat = new cv.Mat(height, width, cv.CV_8UC1);
+               cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY);
+
+               // calc Histgram
+               const matVector = new cv.MatVector();
+               matVector.push_back(gray);
                
+               cv.calcHist(matVector, channels, new cv.Mat(), hist, histSize, ranges);
+               cv.normalize(hist, hist, 1, 0, cv.NORM_L1);
+
                // transform
-               cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY);
-     
-               // draw image
-               cv.imshow(canvas, src);
-     
-               // delete image
-               src.delete();
+               if (i > 1) {
+                    console.log("hist: ", hist);
+                    console.log("hist2: ", hist2);
+                    similarity = cv.compareHist(hist, hist2, 0);
+                    console.log("similarity: ", similarity);
+               } else {
+                    hist2 = hist.clone();
+               }
      
                // save image
-               result.appendChild(canvas);
-               
+               if (i == 1 || similarity < 0.70){
+                    // draw image
+                    hist2 = hist.clone();
+
+                    // add frame number
+                    context.fillStyle = 'rgb(0, 0, 0)';
+                    context.fillRect(width * 0.9, height * 0.9, 
+                                        width * 0.95, height * 0.95);
+                    context.font = '10px serif';
+                    context.fillStyle = 'rgb(255, 255, 255)';
+                    context.fillText(String(i), width * 0.92 , height * 0.98);
+                    src.data.set(context.getImageData(0, 0, width, height).data);
+
+                    cv.imshow(canvas, src);
+                    result.appendChild(canvas);
+               }
+
+               // delete image
+               src.delete();
+               gray.delete();
+
                // update
                video.currentTime += delay;
                i += 1;
           }
-     }, 0);    
+     }, 0);  
 }
 
 function getVideo(){
